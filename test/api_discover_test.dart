ac58193430a6f5d16e55bbf748db53bcafc124bf
Sebastian Müller <sebastian.m@affinidi.com>
2025-11-11 18:42:30 +0100

    chore: use control plane DID only in QR code

--- test/api_discover_test.dart
+++ test/api_discover_test.dart
@@ -1,37 +1,15 @@
 import 'dart:io';
-
-import 'package:meeting_place_control_plane_api/src/core/config/env_config.dart';
-import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:dio/dio.dart';
 import 'package:test/test.dart';
+import 'package:meeting_place_control_plane_api/src/core/config/env_config.dart';
 
 void main() {
   final apiEndpoint = getEnv('API_ENDPOINT');
   final dio = Dio();
 
   test('#discovery: success', () async {
-    final response = await dio.get(
-      '$apiEndpoint/discover',
-      options: Options(headers: {
-        Headers.contentTypeHeader: 'application/json',
-      }),
-    );
-
-    expect(response.statusCode, HttpStatus.ok);
-
-    final payload = JWT.decode(response.data['token']);
-    final payloadData = payload.payload['data'];
-
-    expect(payload.issuer, getEnv('CONTROL_PLANE_DID'));
-    expect(
-      payloadData['type'],
-      'https://affinidi.io/mpx/control-plane/registration',
-    );
-
-    expect(payloadData['from'], getEnv('CONTROL_PLANE_DID'));
-    expect(payloadData['body'], {
-      'httpApi': getEnv('API_ENDPOINT'),
-      'httpApiVersion': 'v1',
-    });
+    final response = await dio.get('$apiEndpoint/discover');
+    expect(response.statusCode, equals(HttpStatus.ok));
+    expect(response.data, equals(getEnv('CONTROL_PLANE_DID')));
   });
 }
